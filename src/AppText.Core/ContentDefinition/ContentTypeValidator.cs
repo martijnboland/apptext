7d13c3e160d327b27e08127862626f8017c6a652
Martijn Boland <martijn@boland.org>
2018-11-21 19:49:37 +0100

    Added content type duplicate name validation

--- src/AppText.Core/ContentDefinition/ContentTypeValidator.cs
+++ src/AppText.Core/ContentDefinition/ContentTypeValidator.cs
@@ -8,10 +8,12 @@ namespace AppText.Core.ContentDefinition
     public class ContentTypeValidator : Validator<ContentType>
     {
         private readonly IApplicationStore _applicationStore;
+        private readonly IContentDefinitionStore _contentDefinitionStore;
 
-        public ContentTypeValidator(IApplicationStore applicationStore)
+        public ContentTypeValidator(IApplicationStore applicationStore, IContentDefinitionStore contentDefinitionStore)
         {
             _applicationStore = applicationStore;
+            _contentDefinitionStore = contentDefinitionStore;
         }
 
         protected override void ValidateCustom(ContentType objectToValidate)
@@ -23,6 +25,15 @@ namespace AppText.Core.ContentDefinition
                 if (app == null)
                 {
                     AddError(new ValidationError { Name = "App", ErrorMessage = "AppText:AppNotFound", Parameters = new[] { objectToValidate.App.Id } } );
+                }
+            }
+
+            // Duplicate content type name
+            if (objectToValidate.Id == null)
+            {
+                if (_contentDefinitionStore.GetContentTypes(new ContentTypeQuery { AppPublicId = objectToValidate.App.PublicId, Name = objectToValidate.Name }).Length > 0)
+                {
+                    AddError(new ValidationError { Name = "Name", ErrorMessage = "AppText:DuplicateContentTypeName", Parameters = new[] { objectToValidate.Name } });
                 }
             }
         }
