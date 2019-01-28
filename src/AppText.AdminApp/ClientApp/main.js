2dde392e99326234e2771f48547e811fff035fd5
Martijn Boland <martijn@boland.org>
2019-01-28 23:21:59 +0100

    Added webpack build to AppText.Admin

--- src/AppText.AdminApp/ClientApp/main.js
+++ src/AppText.AdminApp/ClientApp/main.js
@@ -1,3 +1,5 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+import './styles/test.css';
+
+document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('dynamic').innerHTML = new Date().toLocaleString() + '  ' + window.appText.apiBaseUrl;
 });
---
+++ src/AppText.AdminApp/ClientApp/webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env = {}, argv = {}) => {
+
+  const isProd = argv.mode === 'production';
+
+  const config = {
+    mode: argv.mode || 'development', // we default to development when no 'mode' arg is passed
+    entry: {
+      main: './main'
+    },
+    output: {
+      path: path.resolve(__dirname, '../wwwroot/dist'),
+      filename: '[name].[chunkhash].js',
+      publicPath: '~/dist'
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'postcss-loader', // Run post css actions
+              options: {
+                plugins: function () { // post css plugins, can be exported to postcss.config.js
+                  return [
+                    require('precss'),
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+        {
+          // Transpile .ts, .tsx and .js files
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          // exclude node_modules
+          exclude: /node_modules/
+        },
+        { 
+          test: /\.(png|jpg|jpeg|gif|svg)$/, 
+          loader: 'url-loader?limit=8192' 
+        },
+        { 
+          test: /\.(woff|woff2|eot|ttf)(\?|$)/, 
+          loader: 'url-loader?limit=30000' 
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[chunkhash].css'
+      }),
+      new HtmlWebpackPlugin({
+        filename: '../../Views/Admin/AppTextAdmin.cshtml',
+        inject: 'body',
+        template: './templates/AppTextAdminTemplate.cshtml'
+      }),
+    ]
+  };
+
+  // Set environment-specific properties
+  if (isProd) {
+    config.devtool = 'source-map';
+  } else { // development
+    config.devtool = 'eval-source-map';
+  }
+
+  return config;
+};
