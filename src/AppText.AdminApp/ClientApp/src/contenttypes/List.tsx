c8310d4e46e0c79739b1c39f864c342c06906e34
Martijn Boland <martijn@boland.org>
2019-04-15 21:56:11 +0200

    Navigate to content type editor from list

--- src/AppText.AdminApp/ClientApp/src/contenttypes/List.tsx
+++ src/AppText.AdminApp/ClientApp/src/contenttypes/List.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom'; 
 import { ContentType } from './models';
 
-interface ListProps {
+interface ListProps extends RouteComponentProps<{}> {
   contentTypes: ContentType[]
 }
 
-const List: React.FunctionComponent<ListProps> = ({ contentTypes }) => {
+const List: React.FunctionComponent<ListProps> = ({ contentTypes, match }) => {
   return (
     <>
       <h1>Content types</h1>
       <dl>
         {contentTypes.map(ct => 
           <React.Fragment key={ct.id}>
-            <dt>{ct.name}</dt>
+            <dt><Link to={{ pathname: `${match.url}/edit/${ct.id}` }}>{ct.name}</Link></dt>
             <dd>{ct.description}</dd>
           </React.Fragment>
         )}
