5b052b3bff2163029dbe9a3678890dbfb48b68ee
Martijn Boland <martijn@boland.org>
2020-06-02 21:32:59 +0200

    Added auth options for Translations add-on

--- src/AppText.Translations/Configuration/AppTextBuilderExtensions.cs
+++ src/AppText.Translations/Configuration/AppTextBuilderExtensions.cs
@@ -11,8 +11,26 @@ namespace AppText.Translations.Configuration
 {
     public static class AppTextBuilderExtensions
     {
-        public static AppTextBuilder AddTranslations(this AppTextBuilder appTextBuilder)
+        public static AppTextBuilder AddTranslations(this AppTextBuilder appTextBuilder, Action<AppTextTranslationsConfigurationOptions> setupAction = null)
         {
+            // Set options
+            var enrichOptions = setupAction ?? delegate { };
+            var options = new AppTextTranslationsConfigurationOptions();
+            enrichOptions(options);
+
+            var serviceProvider = appTextBuilder.Services.BuildServiceProvider();
+            var appTextConfiguration = serviceProvider.GetService<AppTextPublicConfiguration>();
+            
+            // Try to inherit auth options from api configuration when not set
+            if (!options.RequireAuthenticatedUser.HasValue)
+            {
+                options.RequireAuthenticatedUser = appTextConfiguration.RequireAuthenticatedUser;
+            }
+            if (options.RequiredAuthorizationPolicy == null)
+            {
+                options.RequiredAuthorizationPolicy = appTextConfiguration.RequiredAuthorizationPolicy;
+            }
+
             // Add translations initializer.
             appTextBuilder.Services.AddTransient<IStartupFilter>(sp => new TranslationsInitializer(sp));
 
@@ -22,27 +40,28 @@ namespace AppText.Translations.Configuration
             mvcBuilder.AddApplicationPart(assembly);
 
             // Try to find a route prefix for the AppText api. When found, use that one also for the controllers of this assembly. Otherwise leave empty.
-            var serviceProvider = appTextBuilder.Services.BuildServiceProvider();
-            var appTextConfiguration = serviceProvider.GetService<AppTextPublicConfiguration>();
-
             var translationsPrefix = appTextConfiguration != null ? appTextConfiguration.RoutePrefix : String.Empty;
 
             mvcBuilder.AddMvcOptions(mvcOptions =>
             {
                 mvcOptions.Conventions.Insert(0, new AppTextRouteConvention(translationsPrefix, assembly));
+                mvcOptions.Conventions.Add(new AppTextAuthorizationConvention(
+                    options.RequireAuthenticatedUser.HasValue ? options.RequireAuthenticatedUser.Value : false,
+                    options.RequiredAuthorizationPolicy,
+                    assembly));
             });
 
             // Add the custom output formatters
-            mvcBuilder.AddMvcOptions(options =>
+            mvcBuilder.AddMvcOptions(mvcOptions =>
             {
-                options.OutputFormatters.Insert(0, new TranslationResultJsonFormatter());
-                options.OutputFormatters.Add(new TranslationResultResxFormatter());
-                options.OutputFormatters.Add(new TranslationResultPoFormatter());
+                mvcOptions.OutputFormatters.Insert(0, new TranslationResultJsonFormatter());
+                mvcOptions.OutputFormatters.Add(new TranslationResultResxFormatter());
+                mvcOptions.OutputFormatters.Add(new TranslationResultPoFormatter());
             });
-            mvcBuilder.AddFormatterMappings(options =>
+            mvcBuilder.AddFormatterMappings(formatterOptions =>
             {
-                options.SetMediaTypeMappingForFormat("resx", "text/microsoft-resx");
-                options.SetMediaTypeMappingForFormat("po", "text/x-gettext-translation");
+                formatterOptions.SetMediaTypeMappingForFormat("resx", "text/microsoft-resx");
+                formatterOptions.SetMediaTypeMappingForFormat("po", "text/x-gettext-translation");
             });
 
             return appTextBuilder;
---
+++ src/AppText.Translations/Configuration/AppTextTranslationsConfigurationOptions.cs
@@ -0,0 +1,15 @@
+namespace AppText.Translations.Configuration
+{
+    public class AppTextTranslationsConfigurationOptions
+    {
+        /// <summary>
+        /// Does the app require an authenticated user? When left empty, the possible value of the API configuration is used. 
+        /// </summary>
+        public bool? RequireAuthenticatedUser { get; set; }
+
+        /// <summary>
+        /// A pre-defined authorization policy (name) that is applied to the API. When left empty, the possible value of the API configuration is used.
+        /// </summary>
+        public string RequiredAuthorizationPolicy { get; set; }
+    }
+}
