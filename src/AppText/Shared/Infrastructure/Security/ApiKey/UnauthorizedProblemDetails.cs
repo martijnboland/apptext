99fc9ec9f5b36a41654b12a42ff03fc50d43ed5a
Martijn Boland <martijn@boland.org>
2020-07-27 11:10:58 +0200

    WIP: API key auth interfering with regular auth. Trying to fix that...

---
+++ src/AppText/Shared/Infrastructure/Security/ApiKey/UnauthorizedProblemDetails.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppText.Shared.Infrastructure.Security.ApiKey
+{
+    public class UnauthorizedProblemDetails : ProblemDetails
+    {
+        public UnauthorizedProblemDetails(string details = null)
+        {
+            Title = "Unauthorized";
+            Detail = details;
+            Status = 401;
+            Type = "https://httpstatuses.com/401";
+        }
+    }
+}
