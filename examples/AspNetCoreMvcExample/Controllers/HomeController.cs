1044b4e80737e6f1a030c86f39305eef28004d1a
Martijn Boland <martijn@boland.org>
2020-07-31 15:13:58 +0200

    Added ASP.NET Core example app

---
+++ examples/AspNetCoreMvcExample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCoreMvcExample.Models;
+using Microsoft.Extensions.Localization;
+
+namespace AspNetCoreMvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = _localizer["AppText ASP.NET Core MVC Example"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
