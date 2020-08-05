a86d0f2fdb0e4e5c0fab3ddf9302e5e9530bfc4a
Martijn Boland <martijn@boland.org>
2020-08-05 14:16:14 +0200

    Added clear button to content search field

--- src/AppText.AdminApp/ClientApp/src/content/ContentLocator.tsx
+++ src/AppText.AdminApp/ClientApp/src/content/ContentLocator.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Field, Formik } from 'formik';
-import { TextInput, Select, SelectOption } from '../common/components/form';
+import { Field, Formik, getIn } from 'formik';
+import { Select, SelectOption } from '../common/components/form';
+import { FaTimes } from 'react-icons/fa';
 
 import { Collection } from '../collections/models';
 import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
 
 interface ContentLocatorProps {
   collections: Collection[],
@@ -36,10 +38,36 @@ const ContentLocator: React.FC<ContentLocatorProps> = ({ collections, collection
       onSubmit={onSubmit}
     >
       {({handleSubmit}) => (
-        <form onSubmit={handleSubmit}>
+        <form className="w-75" onSubmit={handleSubmit}>
           <div className="form-row">
-            <Field name="collectionId" label={t('Labels:Collection')} className="form-group col-md-6" component={Select} options={collectionOptions} onChange={onCollectionChanged} />
-            <Field name="searchTerm" label={t('Labels:KeyStartsWith')} className="form-group col-md-6" component={TextInput} />
+            <Field name="collectionId" label={t('Labels:Collection')} className="form-group col-md-4" component={Select} options={collectionOptions} onChange={onCollectionChanged} />
+            <Field name="searchTerm">
+              {({
+                field: { name, value, ...restField }, // { name, value, onChange, onBlur }
+                form: { errors, setFieldValue, submitForm }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+               }) => {
+                const error = getIn(errors, name);
+
+                const onClearSearchText = () => {
+                  setFieldValue('searchTerm', '');
+                  submitForm();
+                }              
+              
+                 return (
+                  <div className="form-group col-md-8">
+                    <label>{t('Labels:KeyStartsWith')}</label>
+                    <div className="input-group">
+                      <input type="text" name={name} value={value||''} {...restField} className={classNames('form-control', 'w-25', { 'is-invalid': error })} />
+                      <div className="input-group-append">
+                        <button className="btn btn-primary" type="button" onClick={onClearSearchText}><FaTimes /></button>
+                      </div>
+                    </div>
+                    {error && <div className="invalid-feedback">{error}</div>}
+                  </div>
+                 )
+               }
+              }
+            </Field>
           </div>
         </form>
       )}
