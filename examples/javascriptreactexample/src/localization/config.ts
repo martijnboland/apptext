1fc2d0662834581ccea5ed28e1a807365c68d49e
Martijn Boland <martijn@boland.org>
2020-10-27 10:37:03 +0100

    Added default api-key that is in the apptextio/apptext-demo docker image

--- examples/javascriptreactexample/src/localization/config.ts
+++ examples/javascriptreactexample/src/localization/config.ts
@@ -1,4 +1,4 @@
 export const currentLanguageStorageKey = 'CURRENT_LANGUAGE';
 export const appTextApiBaseUrl = process.env.REACT_APP_APPTEXT_API_BASEURL || 'http://localhost:8000';
 export const appTextAppId = process.env.REACT_APP_APPTEXT_APP_ID || 'jsexample';
-export const appTextApiKey = process.env.REACT_APP_APPTEXT_APIKEY || '';
+export const appTextApiKey = process.env.REACT_APP_APPTEXT_APIKEY || '817ea5679a3c4b0fb5cd0e55d921ef19';
