edc86551d8888b2acbd6b862485e9e1d83446adb
Martijn Boland <martijn@boland.org>
2023-01-31 10:08:23 +0100

    Updated ASP.NET example project to net6.0

--- examples/AspNetCoreMvcExample/Program.cs
+++ examples/AspNetCoreMvcExample/Program.cs
@@ -1,26 +1,58 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using AppText.AdminApp.Configuration;
+using AppText.Configuration;
+using AppText.Features.Application;
+using AppText.Localization;
+using AppText.Storage.NoDb;
 
-namespace AspNetCoreMvcExample
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Add AppText
+var dataPath = Path.Combine(builder.Environment.ContentRootPath, "App_Data");
+
+builder.Services.AddAppText()
+    // Use simple JSON file storage
+    .AddNoDbStorage(dataPath)
+    // Admin api is available at /apptext
+    .AddAdmin()
+    // Initialize an app with languages and default language in AppText where the content is stored
+    .InitializeApp("example_app", "ASP.NET Core example", new string[] { "en", "nl", "fr" }, "en")
+    // Add AppText implementation for ASP.NET Core localization 
+    .AddAppTextLocalization(options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        // The ID of the app that we created just before
+        options.AppId = "example_app";
+        // Create empty items in AppText for all keys that are not found
+        options.CreateItemsWhenNotFound = true;
+        // Use AppText app settings of example_app for ASP.NET Core request localization
+        // SupportedCultures becomes 'en', 'nl' and 'fr' and the DefaultCulture is 'en'
+        options.ConfigureRequestLocalizationOptions = true;
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// ASP.NET Core MVC
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseRequestLocalization();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
